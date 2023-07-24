91734b9617d47c916961648c8edae986a35b0bbf
mohammad <mohammadz270@yahoo.com>
2023-07-24 17:49:35 +0430

    Post model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model) :
+    title=models.CharField(max_length=225)
+    category=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='image/')
+    created_time=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now=True)
+    content=models.TextField()
+    autor=models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.title
